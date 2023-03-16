324e6021acfc1d8a759319f3e737b0c8648d697e
Yokiruno <nikkozlovskiy98@gmail.com>
2023-03-16 20:06:14 +0300

    last commit

--- rebase.sh
+++ rebase.sh
@@ -4,10 +4,14 @@
 count=1
 for param in "$@"; do
 <<<<<<< HEAD
+<<<<<<< HEAD
 	echo "\#@ Parameter #$count = $param"
 =======
 	echo "Parameter: $param"
 >>>>>>> 64ccef3 (git-rebase 1)
+=======
+	echo "Next parameter: $param"
+>>>>>>> 8bf5ac6 (git-rebase 2)
 	count=$(( $count + 1 ))
 done
 
